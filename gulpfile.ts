5bf714580a3192e3e6755768f9a0edf6aa5f6dca
JD <jean.daniel.michaud@gmail.com>
2016-06-23 22:00:24 +0200

    Some updates.

--- gulpfile.ts
+++ gulpfile.ts
@@ -23,23 +23,41 @@ gulp.task("tslint", () =>
     .pipe(tslint.report("verbose"))
 );
 
-gulp.task('build.dev', ['tslint', 'clean'], () => {
+function bundleTs() {
   return gulp.src(tsfiles)
     .pipe(sourcemaps.init()) // This means sourcemaps will be generated
     .pipe(ts(tsProject)) // compile to javascript
     .js // Stream javascript
     .pipe(concat('app.js')) // Concatenate javascript
+}
+
+gulp.task('bundle.ts.dev', ['tslint'], () => {
+  return bundleTs()
     .pipe(sourcemaps.write()) // Write the sourcemaps
     .pipe(gulp.dest(dest + '/js'));
 });
 
-gulp.task('build.prod', ['tslint', 'clean'], () => {
-  return gulp.src(tsfiles)
-    .pipe(sourcemaps.init()) // This means sourcemaps will be generated
-    .pipe(ts(tsProject)) // compile to javascript
-    .js // Stream javascript
-    .pipe(concat('app.js')) // Concatenate javascript
+gulp.task('bundle.ts.prod', ['tslint'], () => {
+  return bundleTs()
     .pipe(uglify()) // Minify and uglify
     .pipe(sourcemaps.write()) // Write the sourcemaps
     .pipe(gulp.dest(dest + '/js'));
+});
+
+gulp.task('bundle.html', () => {
+});
+
+gulp.task('bundle.css.dev', () => {
+});
+
+gulp.task('bundle.css.prod', () => {
+});
+
+gulp.task('bundle.img', () => {
+});
+
+gulp.task('build.dev', ['clean', 'bundle.ts.dev'], () => {
+});
+
+gulp.task('build.prod', ['clean', 'bundle.ts.prod'], () => {
 });
